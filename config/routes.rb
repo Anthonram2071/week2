d2e8537d4a5a4c0ef78c9ddec5ffcc52286da019
anthonyram2071 <anthonyjohnramirez@gmail.com>
2022-03-25 11:37:42 -0700

    User SignUp

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  
-
+ 
   resources :posts do
     resources :comments
   end
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  
+  resources :users
+  resources :sessions
+  
 
   root "posts#index"
 end
